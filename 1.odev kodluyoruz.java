5b0516037a6d8dc0ce710f38b0a2c9fe86537b21
Yasminhk <86046401+Yasminhk@users.noreply.github.com>
2021-07-04 09:48:13 +0300

    Create 1.odev kodluyoruz.java
    Hafta 1 
    1.ödev 
    kodluyoruz

---
+++ 1.odev kodluyoruz.java
@@ -0,0 +1,31 @@
+/*	int s1 = 0, s2 = 1, sonuc, i;
+        		System.out.println(s1);
+        		System.out.println(s2);
+             	for (i = 0; i < 10; i++) {
+        		sonuc = s1 + s2;
+        			s1 = s2;
+        			s2 = sonuc;
+        			System.out.println(sonuc);
+            */
+
+             /*  Manav kasa programı
+              float a =2.14f;
+        		float e =3.67f;
+        		float d = 1.11f;
+        		float m=0.95f;
+        		float p = 5.00f ;
+        		float tutar ;
+        		System.out.println("Armut kaç kilo ? " +a + "TL");
+        		System.out.println("Elma kaç kilo ? " +e+ "TL");
+        		System.out.println("Domates kaç kilo ? " +d+ "TL");
+        		System.out.println("Muz kaç kilo ? " +m+ "TL");
+        		System.out.println("Patlıcan  kaç kilo ? " +p+ "TL");
+        		tutar = a+e+d+m+p ;
+        		System.out.println( "Toplam tutar :  "+ tutar+ "  TL");
+              */
+        		// puanlarının harf karşılığı
+        		int n = 66 ;
+        		if ( n < 35 ) System.out.println( "FF");
+        		else if ( n >=35 && n <=50 ) System.out.println( "DC");
+        	else if ( n >50 && n <=70 ) System.out.println( "BB");
+        		else System.out.println( "AA");
